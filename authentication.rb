02ea1a6cef7b9b20395aa36bca9dabde575f975e
Solam <solamnyangiwe@gmail.com>
2024-01-04 16:54:12 +0200

    authentication

--- authentication.rb
+++ authentication.rb
@@ -4,5 +4,20 @@ require_relative 'userModel'
 class Authentication 
     def self.register(username, email, password)
         user = User.new(username: username, email: email, password: password)
+        if user.valid?
+            user.save
+            puts "Registration successful"
+        else
+            puts "Registration failed. #{user.errors.full_messages.join(', ')}"
+        end
+    end
+
+    def self.login(username, password)
+        user = User.find(username: username)
+        if user && user.authenticate(password)
+            puts "Login successful!"
+          else
+            puts "Login failed. Invalid username or password."
+        end
     end
 end
