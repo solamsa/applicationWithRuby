16841f98021238f4e53939d33e2f75b09de9c30c
Solam <solamnyangiwe@gmail.com>
2024-01-05 10:21:36 +0200

    database error

--- database.rb
+++ database.rb
@@ -1,13 +1,22 @@
 require 'pg'
 require 'sequel'
-DB = Sequel.connect(adapter: 'postgres', database: 'testDB', user: 'postgres', password: 'test123', host: '127.0.0.1')
+require_relative 'connectDatabase.rb'
 
-DB.create_table :users do
+begin
+  DB.create_table :users do
     primary_key :id
     String :username, size: 255, null: false
     String :email, size: 255, null: false
     String :password_digest, size: 255, null: false
-    end
+  end
+rescue Sequel::DatabaseError => e 
+  puts e.message
+else
+  puts "Sucessfully added users table!"
+ensure
+  DB.disconnect
+end
+
 # users = DB[:users]
 # users.insert(username: 'john_doe', email: 'john@example.com')
 # result = users.all
