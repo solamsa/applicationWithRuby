5df54b8bd9206e7f8b7eaad335ba4e3204540afc
Solam <solamnyangiwe@gmail.com>
2024-01-05 10:26:31 +0200

    main cleanup

--- main_app.rb
+++ main_app.rb
@@ -1,30 +1,32 @@
 require_relative 'authentication'
+
 loop do
-    puts "1. Register"
-    puts "2. Login"
-    puts "3. Exit"
-
-    choice = gets.chomp
+  puts "1. Register"
+  puts "2. Login"
+  puts "3. Exit"
+  choice = gets.chomp
     
-    case choice
-    when '1'
-        print "Enter username: "
-        username = gets.chomp
-        print "Enter email: "
-        email = gets.chomp
-        print "Enter password: "
-        password = gets.chomp
+  case choice
+  when '1'
+    print "Enter username: "
+    username = gets.chomp
+    print "Enter email: "
+    email = gets.chomp
+    print "Enter password: "
+    password = gets.chomp
+    Authentication.register(username, email, password)
 
-        Authentication.register(username, email, password)
-    when '2'
-        print "Enter username: "
-        username = gets.chomp
-        print "Enter password: "
-        password = gets.chomp
-        Authentication.login(username, password)
-    when '3'
-        break
-      else
-        puts "Invalid choice. Please try again."
-      end
+  when '2'
+    print "Enter username: "
+    username = gets.chomp
+    print "Enter password: "
+    password = gets.chomp
+    Authentication.login(username, password)
+
+  when '3'
+    break
+
+  else
+    puts "Invalid choice. Please try again."
+  end
 end
