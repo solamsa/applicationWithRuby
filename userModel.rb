72f5a6b1eb3c848834c1da7a47336cefa76ff551
Solam <solamnyangiwe@gmail.com>
2024-01-05 10:25:27 +0200

    userModel clean up

--- userModel.rb
+++ userModel.rb
@@ -1,24 +1,24 @@
 require 'sequel'
 require 'bcrypt'
 require 'pg'
-DB = Sequel.connect(adapter: 'postgres', database: 'testDB', user: 'postgres', password: 'test123', host: '127.0.0.1')
+require_relative 'connectDatabase.rb'
+
 Sequel::Model.plugin :validation_helpers
-
 class User < Sequel::Model
-    def validate
-        super
-        validates_presence :username
-        validates_unique :username
-        validates_presence :email
-        validates_unique :email
+  def validate
+    super
+    validates_presence :username
+    validates_unique :username
+    validates_presence :email
+    validates_unique :email
     end
 
-    def password=(password)
-        @password_digest = password
-        self.password_digest = BCrypt::Password.create(password_digest)
-    end
+  def password=(password)
+    @password_digest = password
+    self.password_digest = BCrypt::Password.create(password_digest)
+  end
 
-    def authenticate(password)
-        BCrypt::Password.new(password_digest) == password
-      end
+  def authenticate(password)
+    BCrypt::Password.new(password_digest) == password
+  end
 end
