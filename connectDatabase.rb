6f491e08123d7738b78e289a8b2e65eba58b1132
Solam <solamnyangiwe@gmail.com>
2024-01-05 15:27:35 +0200

    db class

--- connectDatabase.rb
+++ connectDatabase.rb
@@ -1,2 +1,7 @@
 require 'sequel'
-DB = Sequel.connect(adapter: 'postgres', database: 'testDB', user: 'postgres', password: 'test123', host: '127.0.0.1')
+
+class ConnectDB
+  def self.connect
+    Sequel.connect(adapter: 'postgres', database: 'testDB', user: 'postgres', password: 'test123', host: '127.0.0.1')
+  end
+end
