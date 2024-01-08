6a2365fe6ad579e3a4b1ca3a972b715dd23a7ea2
Solam <solamnyangiwe@gmail.com>
2024-01-08 13:00:47 +0200

    seperate db queries

--- database.rb
+++ database.rb
@@ -3,23 +3,41 @@ require 'sequel'
 require_relative 'connectDatabase.rb'
 
 DB = ConnectDB.connect
-begin
-  DB.create_table :users do
-    primary_key :id
-    String :username, size: 255, null: false
-    String :email, size: 255, null: false
-    String :password_hash, size: 255, null: false
+
+module SQL
+
+  class CreateTables
+    def self.create_users
+      begin
+        DB.create_table :users do
+          primary_key :id
+          String :username, size: 255, null: false
+          String :email, size: 255, null: false
+          String :password_hash, size: 255, null: false
+        end
+      rescue Sequel::DatabaseError => e 
+        puts e.message
+      else
+        puts "Sucessfully added users table!"
+      ensure
+        DB.disconnect
+      end
+    end
   end
-rescue Sequel::DatabaseError => e 
-  puts e.message
-else
-  puts "Sucessfully added users table!"
-ensure
-  DB.disconnect
+
+  class ManupulateData
+    def self.remove_user_by_username(name_to_remove)
+      begin
+        users_table = DB[:users]
+        users_table.where(username: name_to_remove).delete
+        puts "Sucessfully deleted #{name_to_remove} from table!"
+      rescue Sequel::DatabaseError => e 
+        puts e.message
+      ensure
+        DB.disconnect
+      end
+    end
+  end
 end
 
-# users = DB[:users]
-# users.insert(username: 'john_doe', email: 'john@example.com')
-# result = users.all
-# puts result
 
