7d7e85ab61d1e114001efdf5d331266626b38d10
Solam <solamnyangiwe@gmail.com>
2024-01-10 15:11:45 +0200

    hiding api keys

--- aws/connectUser.rb
+++ aws/connectUser.rb
@@ -1,8 +1,15 @@
 require 'aws-sdk'
 
+# file_path = File.join(Dir.home, 'aws_credentials.json')
+file_content = File.read('/home/solam/.aws/aws_credentials.json')
+credentials_data = JSON.parse(file_content)
+
+# Extract AWS credentials
+access_key = credentials_data['access_key']
+secret_key = credentials_data['secret_key']
 Aws.config.update({
   region: 'af-south-1',
-  credentials: Aws::Credentials.new('AKIA6GBMFG5X7FSKIBKG', '6j8UfBNDsjkTWI+6iwsFGdOAGSjjeTyJXYL7XA6T')
+  credentials: Aws::Credentials.new(access_key, secret_key)
 })
 
 s3 = Aws::S3::Client.new
