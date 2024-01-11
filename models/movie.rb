9216a337389a29c8ac0321cf3d72163fb493fa09
Solam <solamnyangiwe@gmail.com>
2024-01-11 15:50:15 +0200

    movie set up

---
+++ models/movie.rb
@@ -0,0 +1,18 @@
+require_relative '../requirements.rb'
+require_relative '../connectDatabase.rb'
+
+ConnectDB.connect
+Sequel::Model.plugin :validation_helpers
+class Movie < Sequel::Model
+
+  one_to_many :favourites
+  one_to_many :ratings
+  
+  def validate
+    super
+    validates_presence :imdb_id
+    validates_unique :imdb_id
+    validates_presence :title
+  end
+  
+end
