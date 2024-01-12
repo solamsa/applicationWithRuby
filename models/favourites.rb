504ef82843831d279a14c7646db2768717f37955
Solam <solamnyangiwe@gmail.com>
2024-01-12 15:45:45 +0200

    relationships update

--- models/favourites.rb
+++ models/favourites.rb
@@ -3,8 +3,15 @@ require_relative '../connectDatabase.rb'
 
 class Favourite < Sequel::Model
 
-  many_to_one :users
-  many_to_one :movies
+  many_to_one :user, key: :user_id, class: :User
+  many_to_one :movie, key: :movie_id, class: :Movie
 
   plugin :timestamps, update_on_create: true
+
+  def validate
+    super
+    validates_presence :user
+    validates_presence :movie
+    validates_unique [:user_id, :movie_id]
+  end
 end
