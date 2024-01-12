12abf116cf3ddf181794eff93b82b7ea580e155f
Solam <solamnyangiwe@gmail.com>
2024-01-12 15:48:36 +0200

    relationship with forein key

--- models/rating.rb
+++ models/rating.rb
@@ -2,16 +2,17 @@ require_relative '../requirements.rb'
 require_relative '../connectDatabase.rb'
 
 class Rating < Sequel::Model
+  many_to_one :user, key: :user_id, class: :User
+  many_to_one :movie, key: :movie_id, class: :Movie
   
-  many_to_one :users
-  many_to_one :movies
 
   def validate
     super
-    validates_presence :rating
-    validates_includes (1..5), :rating
     validates_presence :user
     validates_presence :movie
+    validates_presence :rating
+    validates_includes (1..5), :rating
     validates_unique [:user_id, :movie_id]
   end
+
 end
