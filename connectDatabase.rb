ab38433f23b5e8693292059d8ba8dfd0781c248a
Solam <solamnyangiwe@gmail.com>
2024-01-12 15:44:42 +0200

    db connect

--- connectDatabase.rb
+++ connectDatabase.rb
@@ -3,5 +3,6 @@ require_relative 'requirements.rb'
 class ConnectDB
   def self.connect
     Sequel.connect(adapter: 'postgres', database: 'testDB', user: 'postgres', password: 'test123', host: '127.0.0.1')
+    puts "Db connected"
   end
 end
