46d080177182e253bb9f4f229f0c1f0d76c8a209
Solam <solamnyangiwe@gmail.com>
2024-01-12 15:54:02 +0200

    model and foreign keys

---
+++ spec/models/favourite_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require 'rspec'
+
+describe Favourite do
+
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      user =  User.create(username: 'asp_user', email: 'asp_user@example.com', password: 'password')
+      movie = Movie.create(imdb_id: "tt1375665", title: 'Test_Movie', year: "2010")
+      rating = Favourite.new(user: user, movie: movie)
+      expect(rating).to be_valid
+    end
+
+    it 'is invalid without a rating' do
+      user =  User.create(username: 'q_user', email: 'q_user@example.com', password: 'password')
+      movie = Movie.create(imdb_id: "tt1375663", title: 'Tst_Movie', year: "2010")
+      rating = Favourite.new(user: user)
+      expect(rating).to_not be_valid
+    end
+
+  end
+
+  describe 'associations' do
+    it 'belongs to a user' do
+      user =  User.create(username: 'as_user', email: 'as_user@example.com', password: 'password')
+      association = Favourite.association_reflection(:user)
+      expect(association[:type]).to eq :many_to_one
+
+    end
+
+    it 'belongs to a movie' do
+      movie = Movie.create(imdb_id: "tt1375661", title: 'Test_Movie', year: "2010")
+      association = Favourite.association_reflection(:movie)
+      expect(association[:type]).to eq :many_to_one
+    end
+  end
+end
