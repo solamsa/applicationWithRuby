dc96eab3e3cf0033c947dddfe66a404d2a6febb3
Solam <solamnyangiwe@gmail.com>
2024-01-15 15:16:04 +0200

    user session

--- authentication.rb
+++ authentication.rb
@@ -2,10 +2,18 @@ require_relative 'requirements.rb'
 require_relative 'models/userModel'
 
 class Authentication 
+
+  @@current_user = nil
+
+  def self.current_user
+    @@current_user
+  end
+
   def self.register(username, email, password)
     user = User.new(username: username, email: email, password: password)
     if user.valid?
       user.save
+      @@current_user = user
       return "Registration successful"
     else
       return "Registration failed. #{user.errors.full_messages.join(', ')}"
@@ -15,10 +23,17 @@ class Authentication
   def self.login(username, password)
     user = User.find(username: username)
     if user && user.authenticate(password)
+      @@current_user = user
       return "Login successful!"
     else
       return "Login failed. Invalid username or password."
     end
+  end
+
+
+  def self.logout
+    @@current_user = nil
+    return "Logout successful!"
   end
 end
 
