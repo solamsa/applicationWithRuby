ab4565207b0ed46b3795ffba2fb953641bc142c3
Solam <solamnyangiwe@gmail.com>
2024-01-15 15:31:13 +0200

    movie saving controller

---
+++ save_movie.rb
@@ -0,0 +1,15 @@
+require_relative 'requirements.rb'
+require_relative 'models/userModel'
+require_relative 'authentication.rb'
+
+class SaveMovie
+
+  def self.save(movie)
+    if Authentication.current_user && movie.valid?
+      movie.save
+      return "Successfully saved a movie"
+    else
+      return "Save failed, movie already exist"
+    end
+  end
+end
