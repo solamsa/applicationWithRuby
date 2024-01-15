02c6bacd36bdfbe720b0a70f7be0f27ff0b09e74
Solam <solamnyangiwe@gmail.com>
2024-01-15 15:18:00 +0200

    seperation of operations

--- main_app.rb
+++ main_app.rb
@@ -1,34 +1,48 @@
 require_relative 'authentication'
+require 'json'
+require_relative 'app_options'
 
-loop do
-  puts "1. Register"
-  puts "2. Login"
-  puts "3. Exit"
-  choice = gets.chomp
-    
-  case choice
-  when '1'
-    print "Enter username: "
-    username = gets.chomp
-    print "Enter email: "
-    email = gets.chomp
-    print "Enter password: "
-    password = gets.chomp
-    result = Authentication.register(username, email, password)
-    puts result
+class MyApp
 
-  when '2'
-    print "Enter username: "
-    username = gets.chomp
-    print "Enter password: "
-    password = gets.chomp
-    result = Authentication.login(username, password)
-    puts result
+  def runApp
+    loop do
+      puts "1. Register"
+      puts "2. Login"
+      puts "3. Exit"
+      choice = gets.chomp
+        
+      case choice
+      when '1'
+        print "Enter username: "
+        username = gets.chomp
+        print "Enter email: "
+        email = gets.chomp
+        print "Enter password: "
+        password = gets.chomp
+        result = Authentication.register(username, email, password)
+        puts result
 
-  when '3'
-    break
+      when '2'
+        print "Enter username: "
+        username = gets.chomp
+        print "Enter password: "
+        password = gets.chomp
+        result = Authentication.login(username, password)
+        puts result
+        app_options = AppOptions.new
+        app_options.operate
 
-  else
-    puts "Invalid choice. Please try again."
+      when '3'
+        result = Authentication.logout
+        puts result
+        break
+
+      else
+        puts "Invalid choice. Please try again."
+      end
+    end
   end
 end
+
+my_app = MyApp.new
+my_app.runApp
