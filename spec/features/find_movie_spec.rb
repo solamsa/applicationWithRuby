1f821afe6810fbd9e72b00d1612d0af189c2b136
Solam <solamnyangiwe@gmail.com>
2024-01-16 15:48:22 +0200

    find movie test

---
+++ spec/features/find_movie_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require 'rspec'
+require 'stringio'
+
+RSpec.describe FindMovie do
+let(:output) { StringIO.new }
+
+  before do
+    allow($stdout).to receive(:puts) {|msg| output.puts(msg)}
+  end
+
+
+  before(:each) do
+    Authentication.register('b_user','b@gmail.com', 'testpass')
+    Authentication.login('b_user', 'testpass')
+    movie = Movie.new(imdb_id: "tt1375661", title: 'ABC', year: "2010")
+    result = SaveMovie.save(movie)
+  end
+
+  describe '.save' do
+    it 'finds a movie successfully' do
+      find_movie = FindMovie.new
+      result = find_movie.get_movies("ABC%")
+      expect(result).to eq("Successfully found related movies")
+    end
+
+    it 'no match found' do
+      find_movie = FindMovie.new
+      result = find_movie.get_movies("ECD%")
+      expect(result).to eq("No matching movies")
+    end
+  end
+end
+
