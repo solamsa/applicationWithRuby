09085e3460df041989a60f71451f288a06adef91
Solam <solamnyangiwe@gmail.com>
2024-01-17 15:44:09 +0200

    hide message

--- connectDatabase.rb
+++ connectDatabase.rb
@@ -4,7 +4,7 @@ class ConnectDB
   @@db_connection
   def self.connect
     @@db_connection = Sequel.connect(adapter: 'postgres', database: 'testDB', user: 'postgres', password: 'test123', host: '127.0.0.1')
-    puts "Db connected"
+    # puts "Db connected"
   end
 
   def self.connection
