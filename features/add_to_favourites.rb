cdec5b78a644bb86014c2ffef62d0d9113e34209
Solam <solamnyangiwe@gmail.com>
2024-01-17 15:44:41 +0200

    add movie to favourites

--- features/add_to_favourites.rb
+++ features/add_to_favourites.rb
@@ -8,12 +8,10 @@ class AddToFavourites
   def self.save(movie)
     current_user = Authentication.current_user
     if current_user 
-      favourite = Favourite.create(:user current_user,:movie movie)
+      favourite = Favourite.create(user: current_user,movie: movie)
       return "Successfully saved a movie to favourites"
     else
       return "Save failed"
     end
   end
-
-
 end
