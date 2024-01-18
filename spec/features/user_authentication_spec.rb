1cb02211183268126bf30805fa13e1b2941f921e
Solam <solamnyangiwe@gmail.com>
2024-01-18 15:53:11 +0200

    testcase match

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -16,13 +16,13 @@ let(:output) { StringIO.new }
   describe '.register' do
     it 'registers a user successfully' do
       result = Authentication.register('b_user','b@gmail.com', 'testpass')
-      expect(result).to eq("Registration successful")
+      expect(result).to eq("Registration successful ✅")
       # SQL::ManupulateData.remove_user_by_username("test_user")
     end
 
     it 'fails to register a user with invalid data' do
       result = Authentication.register('b_user', 'b@example.com', 'password123')
-      expect(result).to eq("Registration failed. username is already taken")
+      expect(result).to eq("Registration failed. username is already taken ❌")
     end 
   end
 
@@ -30,12 +30,12 @@ let(:output) { StringIO.new }
     it 'logs in successfully with correct credentials' do
       # Assuming you have registered a user with these credentials first
       result = Authentication.login('b_user', 'testpass')
-      expect(result).to eq("Login successful!")
+      expect(result).to eq("Login successful! ✅")
     end
 
     it 'fails to log in with incorrect credentials' do
       result = Authentication.login('nonexistent_user', 'incorrect_password')
-      expect(result).to eq("Login failed. Invalid username or password.")
+      expect(result).to eq("Login failed. Invalid username or password. ❌")
     end
   end
 end
