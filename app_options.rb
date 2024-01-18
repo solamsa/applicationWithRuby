9af37cb385e3d5fdf780e0c1534b9b97d7df8911
Solam <solamnyangiwe@gmail.com>
2024-01-18 15:46:24 +0200

    app engine

--- app_options.rb
+++ app_options.rb
@@ -3,73 +3,41 @@ require_relative 'omdbapi/apiModel'
 require_relative 'features/save_movie'
 require_relative 'models/movie'
 require_relative 'features/find_movie'
+require_relative 'apptools/app_tools'
 require 'pry'
 
 require 'json'
  
 class AppOptions
 
-  @@find_movie = FindMovie.new
-  @@api_model = ApiModel.new('2fee9ad3')
+  @@app_tools = Apptools::Tools.new
   def operate
     loop do
-      puts "1. Add new movie to the system"
-      puts "2. Find movie"
-      puts "3. Exit"
+      puts "1. Add new movie to the app"
+      puts "2. Find movie from the app"
+      puts "3. Add movie to favourites"
+      puts "4. Rate a movie"
+      puts "5. Exit"
       choice = gets.chomp
         
       case choice
       when '1'
-      
-        puts "Search the movie to add"
-        movie_search = gets.chomp
-        response = @@api_model.search_movie(movie_search)
-        i = 0
-      
-        if response['Response'] == 'True'
-          movies = response['Search']
-          movies.each do |movie|
-            puts "=======================================\n"
-            puts "no .#{i} #{movie}\n\n"
-            i = i+1
-        end
-
-        puts "\nSelect the movie to add by inputing it's number"
-        movie_num = gets.chomp.to_i
-        selected_movie = movies[movie_num.to_i]
-        puts "selected movie: #{selected_movie}"
-        movie = Movie.new(imdb_id: selected_movie['imdbID'], title: selected_movie['Title'], year: selected_movie['Year'], type: selected_movie['Type'], poster: selected_movie['Poster'])
-        result = SaveMovie.save(movie)
-        puts result
-        else
-          puts "Error: #{response['Error']}"
-        end
-
+      @@app_tools.search_api
       when '2'
-        puts "\nEnter the name of the movie you want to search"
-        movie_search = gets.chomp
-        movie_search = movie_search + "%"
-        @@find_movie = FindMovie.new
-        result = @@find_movie.find_movies(movie_search)
-        puts "#{result}\n"
-        @@find_movie.show_movies
-        # binding.pry
-        puts "Do you want to add a movie to favourites[y/n]"
-        answer = gets.chomp.downcase
-        if answer == "y" || answer == "yes"
-          @@find_movie.want_add_favourite
-        end
-
-        puts "Do you want to rate a movie[y/n]"
-        answer = gets.chomp.downcase
-        if answer == "y" || answer == "yes"
-          @@find_movie.want_rate
-        end
+        @@app_tools.app_search
       when '3'
-        result = Authentication.logout
-        puts result
+        @@app_tools.app_search
+        if @@app_tools.get_movies.length > 0
+          @@app_tools.add_favourite
+        end
+      when '4'
+        @@app_tools.app_search
+        if @@app_tools.get_movies.length > 0
+          @@app_tools.rate
+        end
+        
+      when '5'
         break
-
       else
         puts "Invalid choice. Please try again."
       end
