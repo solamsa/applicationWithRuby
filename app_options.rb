e8c2c5b9259a387fc248b9c3e3c8b0dfc373377b
Solam <solamnyangiwe@gmail.com>
2024-01-19 09:32:51 +0200

    favourite functionality

--- app_options.rb
+++ app_options.rb
@@ -15,29 +15,41 @@ class AppOptions
   @@app_tools = Apptools::Tools.new
   def operate
     loop do
+      puts "Main Menu".red.bold
       puts "1. Add new movie to the app"
       puts "2. Find movie from the app"
       puts "3. Add movie to favourites"
       puts "4. Rate a movie"
       puts "5. Your favourites"
       puts "6. Logout"
+      puts"\nOption:" 
       choice = gets.chomp
-        
+      puts"\n"   
+
       case choice
       when '1'
       @@app_tools.search_api
       when '2'
         @@app_tools.app_search
       when '3'
-        @@app_tools.app_search
+        begin
+          @@app_tools.app_search
         if @@app_tools.get_movies.length > 0
           @@app_tools.add_favourite
         end
-      when '4'
-        @@app_tools.app_search
-        if @@app_tools.get_movies.length > 0
-          @@app_tools.rate
+        rescue StandardError => e
+          puts "An error occurred: #{e.message}".red
         end
+      when '4'
+        begin
+          @@app_tools.app_search
+          if @@app_tools.get_movies.length > 0
+            @@app_tools.rate
+          end
+        rescue StandardError => e
+          puts "An error occurred: #{e.message}".red
+        end
+
       when '5'
         @@app_tools.favourites
       when '6'
